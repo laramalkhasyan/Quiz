00ded1cbfb27a812f902c75621ed2106c703609f
laramalkhasyan <larisa0820m@gmail.com>
2019-10-17 18:44:00 +0400

    corrected version

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let questions = ["What is the longest that an elephant has lived?", "How many rings are on the Olympic flag?", "How did Spider-Man get his powers?",
-    "In darts, what's the most points you can score with a single throw?", "Which of these animals does NOT appear in the Chinese zodiac?"
-];
-let answers = {
-    0: [17, 49, 86, 94, 86],
-    1: ["None", 4, 5, 7, 5],
-    2: ["Military experiment gone awry", "Born with them", "Woke up with them after a strange dream", "Bitten by a radioactive spider",
+let quiz = [{
+    question: "What is the longest that an elephant has lived?",
+    answers: [17, 49, 86, 94],
+    correct: 86
+},
+{
+    question: "How many rings are on the Olympic flag?",
+    answers: ["None", 4, 5, 7],
+    correct: 5
+},
+{
+    question: "How did Spider-Man get his powers?",
+    answers: ["Military experiment gone awry", "Born with them", "Woke up with them after a strange dream", "Bitten by a radioactive spider"],
+    correct: "Bitten by a radioactive spider"
+},
 
-        "Bitten by a radioactive spider"
-    ],
-    3: [20, 50, 60, 100, 60],
-    4: ["Bear", "Rabbit", "Dragon", "Dog", "Bear"]
-}
+{
+    question: "In darts, what's the most points you can score with a single throw?",
+    answers: [20, 50, 60, 100],
+    correct: 60
+},
+{
+    question: "Which of these animals does NOT appear in the Chinese zodiac?",
+    answers: ["Bear", "Rabbit", "Dragon", "Dog"],
+    correct: "Bear"
+}]
+
+
 let checkedAnswers = [""];
 let currentQuestion = 0;
 let backIsAllowed = 1;
@@ -60,7 +75,7 @@ function next() {
         if (currentQuestion) {
             for (let i = 0; i < checkboxes.length; i++) {
                 if (checkboxes[i].checked) {
-                    checkedAnswers[currentQuestion - 1] = answers[currentQuestion - 1][i];
+                    checkedAnswers[currentQuestion - 1] = quiz[currentQuestion - 1].answers[i];
                 }
             }
 
@@ -69,10 +84,10 @@ function next() {
             document.getElementsByClassName("back-button")[0].style.display = "flex";
         }
 
-        document.getElementsByTagName("h5")[0].innerHTML = currentQuestion + 1 + ") " + questions[currentQuestion];
+        document.getElementsByTagName("h5")[0].innerHTML = currentQuestion + 1 + ") " + quiz[currentQuestion].question;
         let ans = document.getElementsByClassName("content");
         for (let i = 0; i < ans.length; i++) {
-            ans[i].innerHTML = answers[currentQuestion][i];
+            ans[i].innerHTML = quiz[currentQuestion].answers[i];
 
         }
         currentQuestion++;
@@ -111,10 +126,9 @@ function finish() {
     if (isChecked()) {
         for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked) {
-                checkedAnswers[currentQuestion - 1] = answers[currentQuestion - 1][i];
+                checkedAnswers[currentQuestion - 1] = quiz[currentQuestion-1].answers[i];
             }
         }
-        console.log(answers);
         let score = 0;
         document.getElementsByClassName("finished-container")[0].style.display = "flex";
         document.getElementsByClassName("quiz-questions")[0].style.display = "none";
@@ -123,10 +137,10 @@ function finish() {
         let correctAnswers = document.getElementsByClassName("correct-answer");
         let userAnswers = document.getElementsByClassName("user-answer");
         for (let i = 0; i < finishedQuestions.length; i++) {
-            finishedQuestions[i].innerHTML = i + 1 + ") " + questions[i];
-            correctAnswers[i].innerHTML += answers[i][finishedQuestions.length - 1];
+            finishedQuestions[i].innerHTML = i + 1 + ") " + quiz[i].question;
+            correctAnswers[i].innerHTML += quiz[i].correct;
             userAnswers[i].innerHTML += checkedAnswers[i];
-            if (checkedAnswers[i] == answers[i][finishedQuestions.length - 1]) {
+            if (checkedAnswers[i] == quiz[i].correct) {
                 score++;
             }
         }
@@ -139,7 +153,7 @@ function retake() {
     document.getElementsByClassName("finished-container")[0].style.display = "none";
     document.getElementsByClassName("finish-button")[0].style.display = "none";
     document.getElementsByClassName("next-button")[0].style.display = "flex";
-    document.getElementsByClassName("score-num")[0].innerHTML = "";
+    document.getElementsByClassName("score-num")[0].innerHTML = "Your score is: ";
     let correctAnswers = document.getElementsByClassName("correct-answer");
     let userAnswers = document.getElementsByClassName("user-answer");
     for (let i = 0; i < correctAnswers.length; i++) {
